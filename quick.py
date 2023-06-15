a3a05ee0b2df2e73d27ab5e4cc16229d917294a2
kazuyai <g2172026@tcu.ac.jp>
2023-06-15 13:52:51 +0900

    世代数、個体数を入力できるように変更

--- quick.py
+++ quick.py
@@ -1,13 +1,17 @@
 from PIL import Image
 import numpy as np
 
-GENERATIONS_NUM = 200
-POPULATION = 50
 WIDTH = 30
 HEIGHT = 30
 
 # 目的画像の指定
-file_name = input("ファイル名を入力")
+file_name = input("ファイル名を入力\n")
+
+# 世代数を指定
+generation_num = int(input("世代数 : "))
+
+# 1世代の個体数を指定
+population = int(input("1世代の個体数 : "))
 
 # 画像の読み込み
 img_in = Image.open('./Images/' + file_name)
@@ -17,23 +21,23 @@ img_in = img_in.resize((WIDTH, HEIGHT))
 img_out = Image.new("RGB", (WIDTH, HEIGHT))
 
 # 親世代配列
-parent_list = np.empty((POPULATION, HEIGHT, WIDTH, 3), int)
+parent_list = np.empty((population, HEIGHT, WIDTH, 3), int)
 
 # 子世代配列
-child_list = np.empty((POPULATION, HEIGHT, WIDTH, 3), int)
+child_list = np.empty((population, HEIGHT, WIDTH, 3), int)
 
 # スコア配列
-scores = np.empty(POPULATION, int)
+scores = np.empty(population, int)
 
 # ランダムなドットを生成する関数
 def createRandomDots():
     global parent_list
-    parent_list = np.random.randint(0, 256, (POPULATION, HEIGHT, WIDTH, 3))
+    parent_list = np.random.randint(0, 256, (population, HEIGHT, WIDTH, 3))
 
 # スコアを計算する関数
 def calcScore():
     global scores, parent_list
-    scores = np.empty(POPULATION, int)
+    scores = np.empty(population, int)
     # 色差を取得する
     diff = parent_list - np.array(img_in)[np.newaxis, :, :, :3]
     # 各色の差を2乗したものを合計した物をスコアにする
@@ -50,10 +54,10 @@ def createNextGeneration():
     global parent_list, child_list
     
     # 子世代配列を初期化する
-    child_list = np.empty((POPULATION, HEIGHT, WIDTH, 3), int)
+    child_list = np.empty((population, HEIGHT, WIDTH, 3), int)
     
     # 生成
-    for i in range(POPULATION):
+    for i in range(population):
         # 親の選択
         parents = selectParent()
         # 交叉
@@ -69,11 +73,11 @@ def createNextGeneration():
 # 親個体を2つ選択する関数
 def selectParent():
     # (個体数 - 順位)/ 1～個体数の和 で確率を決定
-    total = POPULATION * (POPULATION + 1) / 2
-    probabilities = [(POPULATION - i) / total for i in range(0, POPULATION)]
+    total = population * (population + 1) / 2
+    probabilities = [(population - i) / total for i in range(0, population)]
     
     # 確率に従って異なる親を2つ選択
-    num1, num2 = np.random.choice(POPULATION, size = 2, replace = False, p = probabilities)
+    num1, num2 = np.random.choice(population, size = 2, replace = False, p = probabilities)
     return num1, num2
     
 # リストから画像を作成する関数      
@@ -89,7 +93,7 @@ def makeImage(dots_list):
 # 最初の世代を生成                
 createRandomDots()
 
-for i in range(GENERATIONS_NUM):    
+for i in range(generation_num):    
     calcScore()
     print(i + 1, "世代")
     if (i + 1) % 100 == 0:
